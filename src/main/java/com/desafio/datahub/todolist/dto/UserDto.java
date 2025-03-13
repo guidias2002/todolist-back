1824ee4d74e06133a42bc295fe9ea55978a691d7
guidias2002 <guidias200220@gmail.com>
2025-03-13 19:35:58 -0300

    feat: create UserDto and add validations

---
+++ src/main/java/com/desafio/datahub/todolist/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.desafio.datahub.todolist.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserDto(
+        @NotBlank(message = "O nome é obrigatório")
+        String name,
+
+        @NotBlank(message = "O e-mail é obrigatório")
+        @Email(message = "Formato de e-mail inválido")
+        String email,
+
+        @NotBlank(message = "A senha é obrigatória")
+        String password
+) {}
