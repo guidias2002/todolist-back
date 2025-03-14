ec172a1cd6bdaff9f71adf11ac02b08f26b40bef
guidias2002 <guidias200220@gmail.com>
2025-03-13 23:32:37 -0300

    feat: create method findTasksByStatus

--- src/main/java/com/desafio/datahub/todolist/repository/TaskRepository.java
+++ src/main/java/com/desafio/datahub/todolist/repository/TaskRepository.java
@@ -1,9 +1,14 @@
 package com.desafio.datahub.todolist.repository;
 
 import com.desafio.datahub.todolist.domain.TaskEntity;
+import com.desafio.datahub.todolist.enums.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+
+    List<TaskEntity> findTasksByStatus(TaskStatus status);
 }
