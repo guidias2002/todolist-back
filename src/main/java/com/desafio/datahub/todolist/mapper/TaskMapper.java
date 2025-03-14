522c85220f9365e610efb7af62a3bca2935cb756
guidias2002 <guidias200220@gmail.com>
2025-03-13 22:35:04 -0300

    feat: create methods toTaskDto and toTaskDtoList

---
+++ src/main/java/com/desafio/datahub/todolist/mapper/TaskMapper.java
@@ -0,0 +1,16 @@
+package com.desafio.datahub.todolist.mapper;
+
+import com.desafio.datahub.todolist.domain.TaskEntity;
+import com.desafio.datahub.todolist.dto.TaskDto;
+import com.desafio.datahub.todolist.dto.TaskPostDto;
+import org.mapstruct.Mapper;
+import org.springframework.scheduling.config.Task;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface TaskMapper {
+
+    TaskDto toTaskDto(TaskEntity taskEntity);
+    List<TaskDto> toTaskDtoList(List<TaskEntity> taskEntityList);
+}
