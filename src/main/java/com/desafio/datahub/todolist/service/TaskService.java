33db65aee5d26c825233c1c83a7058775e3ccfb7
guidias2002 <guidias200220@gmail.com>
2025-03-13 23:32:55 -0300

    feat: create methods deleteTaskById and filterTaskByStatus

--- src/main/java/com/desafio/datahub/todolist/service/TaskService.java
+++ src/main/java/com/desafio/datahub/todolist/service/TaskService.java
@@ -3,6 +3,10 @@ package com.desafio.datahub.todolist.service;
 import com.desafio.datahub.todolist.domain.TaskEntity;
 import com.desafio.datahub.todolist.dto.TaskDto;
 import com.desafio.datahub.todolist.dto.TaskPostDto;
+import com.desafio.datahub.todolist.enums.TaskStatus;
+import com.desafio.datahub.todolist.exceptions.InvalidDueDateException;
+import com.desafio.datahub.todolist.exceptions.NotFoundException;
+import com.desafio.datahub.todolist.exceptions.TaskOwnershipException;
 import com.desafio.datahub.todolist.mapper.TaskMapper;
 import com.desafio.datahub.todolist.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,12 +30,10 @@ public class TaskService {
 
 
     public TaskDto createTask(TaskPostDto taskPostDto, Long userId) {
-        if(!userService.existsUserById(userId)) {
-            throw new RuntimeException("Usuário não encontrado.");
-        }
+        userService.findUserById(userId);
 
         if(taskPostDto.dueDate().isBefore(LocalDate.now())) {
-            throw new RuntimeException("A data de vencimento não pode ser anterior à data atual.");
+            throw new InvalidDueDateException("A data de vencimento não pode ser anterior à data atual.");
         }
 
         TaskEntity newTask = new TaskEntity();
@@ -50,5 +52,28 @@ public class TaskService {
     public List<TaskDto> findAllTasks() {
 
         return taskMapper.toTaskDtoList(taskRepository.findAll());
+    }
+
+    public void deleteTaskById(Long taskId, Long userId) {
+        userService.findUserById(userId);
+
+        TaskEntity task = findTaskById(taskId);
+
+        if(!task.getUserId().equals(userId)) {
+            throw new TaskOwnershipException("Apenas o criador da tarefa pode exclui-lá.");
+        }
+
+        taskRepository.deleteById(taskId);
+    }
+
+    public List<TaskDto> filterTaskByStatus(TaskStatus status) {
+
+        return taskMapper.toTaskDtoList(taskRepository.findTasksByStatus(status));
+    }
+
+    public TaskEntity findTaskById(Long taskId) {
+
+        return taskRepository.findById(taskId)
+                .orElseThrow(() -> new NotFoundException("Tarefa não encontrada."));
     }
 }
